52c230303b8b4a9d24f38258b2ef3ca18962e840
ValentinCasas <valen.casas12@gmail.com>
2022-07-06 16:38:41 -0300

    Update Producto.java

--- src/resto/entidades/Producto.java
+++ src/resto/entidades/Producto.java
@@ -4,122 +4,119 @@ import java.text.*;
 
 public class Producto {
 //  Atributos
-  private int codigo;
-  private String nombre;
-  private int cantidad = 1;
-  private double precio;
-  private boolean comestible;
+
+    private int codigo;
+    private String nombre;
+    private int stock;
+    private double precio;
+    private boolean comestible;
+    private boolean activo;
 
 //  Constructor
-  public Producto() {
-  }
+    public Producto() {
+    }
 
-  public Producto(String nombre, double precio, boolean comestible) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.comestible = comestible;
-  }
+    public Producto(int codigo, String nombre, int stock, double precio, boolean comestible, boolean activo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+        this.comestible = comestible;
+        this.activo = activo;
+    }
 
-  public Producto(int codigo, String nombre, double precio, boolean comestible) {
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.comestible = comestible;
-  }
-
-  public Producto(String nombre, int cantidad, double precio, boolean comestible) {
-    this.nombre = nombre;
-    this.cantidad = cantidad;
-    this.precio = precio;
-    this.comestible = comestible;
-  }
-
-  public Producto(int codigo, String nombre, int cantidad, double precio, boolean comestible) {
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.cantidad = cantidad;
-    this.precio = precio;
-    this.comestible = comestible;
-  }
+    public Producto(String nombre, int stock, double precio, boolean comestible, boolean activo) {
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+        this.comestible = comestible;
+        this.activo = activo;
+    }
 
 //  Getters y setters
-  public int getCodigo() {
-    return codigo;
-  }
+    public int getCodigo() {
+        return codigo;
+    }
 
-  public void setCodigo(int codigo) {
-    this.codigo = codigo;
-  }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 
-  public String getNombre() {
-    return nombre;
-  }
+    public String getNombre() {
+        return nombre;
+    }
 
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-  public int getCantidad() {
-    return cantidad;
-  }
+    public int getStock() {
+        return stock;
+    }
 
-  public void setCantidad(int cantidad) {
-    this.cantidad = cantidad;
-  }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 
-  public double getPrecio() {
-    return precio;
-  }
+    public double getPrecio() {
+        return precio;
+    }
 
-  public void setPrecio(double precio) {
-    this.precio = precio;
-  }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
-  public boolean isComestible() {
-    return comestible;
-  }
+    public boolean isComestible() {
+        return comestible;
+    }
 
-  public void setComestible(boolean comestible) {
-    this.comestible = comestible;
-  }
+    public void setComestible(boolean comestible) {
+        this.comestible = comestible;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 
 //  Metodos
-  @Override
-  public String toString() {
-    NumberFormat formatter = new DecimalFormat("#0.00");
+    @Override
+    public String toString() {
+        NumberFormat formatter = new DecimalFormat("#0.00");
 
-    return "==Producto==\n"
-            + "Nombre: " + nombre
-            + "\nCantidad: " + cantidad
-            + "\nPrecio: $" + formatter.format(precio)
-            + "\nCategoria: " + (comestible ? "comestible" : "bebida");
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 7;
-    hash = 61 * hash + this.codigo;
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+        return "==Producto==\n"
+                + "Nombre: " + nombre
+                + "\nStock: " + stock
+                + "\nPrecio: $" + formatter.format(precio)
+                + "\nCategoria: " + (comestible ? "comestible" : "bebida");
     }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final Producto other = (Producto) obj;
-    if (this.codigo != other.codigo) {
-      return false;
-    }
-    return true;
-  }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + this.codigo;
+        return hash;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        return true;
+    }
 
 }
